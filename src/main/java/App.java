47b56dc01f33c13008fa1b3e18699829ca1ae629
Carson Canales and David Cuthell <carson+david@>
2017-08-08 15:34:37 -0700

    ughh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,12 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
         staticFileLocation("/public");
-
+        //get : show new post form
+    get("/posts/new", (req, res) -> {
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView(model, "newpost-form.hbs");
+    }, new HandlebarsTemplateEngine());
+        //post: process new post form
     get("/", (req, res) -> {
         Map<String, Object> model = new HashMap<>();
         ArrayList<Post> posts = Post.getAll();
@@ -18,6 +23,7 @@ public class App {
         return new ModelAndView(model, "index.hbs");
     }, new HandlebarsTemplateEngine());
 
+        //get: show all posts
     post("/posts/new", (request, response) -> { //URL to make new post on POST route
         Map<String, Object> model = new HashMap<String, Object>();
         String content = request.queryParams("content");
@@ -25,6 +31,21 @@ public class App {
         model.put("post", newPost);
         return new ModelAndView(model, "success.hbs");
     }, new HandlebarsTemplateEngine());
+        //get: show an individual post
+    get("/posts/:id", (req, res) -> {
+        Map<String, Object> model = new HashMap<>();
+        int idOfPostToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+        Post foundPost = Post.findById(idOfPostToFind); //use it to find post
+        model.put("post", foundPost); //add it to model for template to display
+        return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+    }, new HandlebarsTemplateEngine());
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete an individual post
+
+        //get: delete all posts
 
     }
 }
